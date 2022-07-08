d62d88931f47d68a23bf3f0b841ea36832410fc4
N4thB <44955424+N4thB@users.noreply.github.com>
2022-07-08 14:48:18 -0500

    main files

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo.shade400,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade600,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int number = 3;
+
+  void changeAnswer() {
+    setState(() {
+      number = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeAnswer();
+              },
+              child: Image.asset('images/ball$number.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
